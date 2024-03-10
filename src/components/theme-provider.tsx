7726cdf7d735233ce9c132944d429c8f13e39eff
adii1203 <aditya32ft@gmail.com>
2024-03-11 01:31:26 +0530

    Add ThemeProvider and useTheme hook

---
+++ src/components/theme-provider.tsx
@@ -0,0 +1,60 @@
+import { useState, useEffect, createContext } from "react";
+
+type Theme = "light" | "dark" | "system";
+
+type ThemeProviderProps = {
+  children: React.ReactNode;
+  defaultTheme?: Theme;
+  storageKey?: string;
+};
+
+type ThemeProviderState = {
+  theme: Theme;
+  setTheme: (theme: Theme) => void;
+};
+
+const initialState: ThemeProviderState = {
+  theme: "system",
+  setTheme: () => null,
+};
+
+export const ThemeContext = createContext<ThemeProviderState>(initialState);
+
+const ThemeProvider = ({
+  children,
+  defaultTheme = "system",
+  storageKey = "vite-ui-theme",
+}: ThemeProviderProps) => {
+  const [theme, setTheme] = useState(
+    () => (localStorage.getItem(storageKey) as Theme) || defaultTheme
+  );
+
+  useEffect(() => {
+    const root = window.document.documentElement;
+    root.classList.remove("light", "dark");
+
+    if (theme === "system") {
+      const systemTheme = window.matchMedia("(prefers-color-scheme: dark)")
+        .matches
+        ? "dark"
+        : "light";
+      root.classList.add(systemTheme);
+      return;
+    }
+    root.classList.add(theme);
+  }, [theme]);
+
+  const value = {
+    theme: theme,
+    setTheme: (theme: Theme) => {
+      localStorage.setItem(storageKey, theme);
+      setTheme(theme);
+    },
+  };
+
+  return (
+    <ThemeContext.Provider value={value}>{children}</ThemeContext.Provider>
+  );
+};
+
+export default ThemeProvider;
--- src/main.tsx
+++ src/main.tsx
@@ -2,12 +2,17 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
-import { ConvexClientProvider } from "./components/convex-provider";
+import { Toaster } from "./components/ui/sonner.tsx";
+import { BrowserRouter } from "react-router-dom";
+import ThemeProvider from "./components/theme-provider.tsx";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <ConvexClientProvider>
-      <App />
-    </ConvexClientProvider>
+    <ThemeProvider defaultTheme="light" storageKey="vite-ui-theme">
+      <BrowserRouter>
+        <App />
+        <Toaster duration={1000} />
+      </BrowserRouter>
+    </ThemeProvider>
   </React.StrictMode>
 );
