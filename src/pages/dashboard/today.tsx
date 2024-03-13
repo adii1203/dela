a7a1b7b085753e92fc8bd8c42f82c3c5dfd6033c
adii1203 <aditya32ft@gmail.com>
2024-03-14 01:12:25 +0530

    Refactor Today component and add tasks functionality

--- src/pages/dashboard/today.tsx
+++ src/pages/dashboard/today.tsx
@@ -1,10 +1,17 @@
-import { Circle, Dot, Square } from "lucide-react";
+import { Dot } from "lucide-react";
 import { useMediaQuery } from "@/hooks/useMediaQuery";
 import { cn } from "@/lib/utils";
 import Creat from "@/components/main/Creat";
+import { useQuery, useMutation, ReactMutation } from "convex/react";
+import { api } from "../../../convex/_generated/api";
+import { Id } from "convex/_generated/dataModel";
+import { toast } from "sonner";
+import { FunctionReference } from "convex/server";
 
 const Today = () => {
   const isMobile = useMediaQuery("(max-width: 768px)");
+  const tasks = useQuery(api.tasks.getAllTask);
+  const check = useMutation(api.tasks.markCompleted);
 
   return (
     <main
@@ -16,17 +23,19 @@ const Today = () => {
         <div className="flex items-center gap-6 capitalize font-bold text-foreground">
           <h1 className="text-5xl">today</h1>
           <span className="outline outline-1 outline-border w-10 h-10 grid place-content-center rounded font-bold text-2xl">
-            5
+            {tasks?.filter((task) => task.isCompleted === false).length}
           </span>
         </div>
         <Creat />
         <section className="mt-6 mx-auto py-3 max-w-[40rem] px-3 rounded-md border-border border min-h-80 bg-card">
-          <ul className="text-card-foreground font-Handlee font-semibold">
-            <p>No tasks for today</p>
-
-            {/* {data?.map((item) => {
-              return ( */}
-            <Task key={"as"} task={"do some work"} isCompleted={true} />
+          <ul className="text-card-foreground font-Lato font-semibold">
+            {tasks?.length === 0 ? (
+              <p>No work for today</p>
+            ) : (
+              tasks?.map((task) => {
+                return <Task check={check} key={task._id} task={task} />;
+              })
+            )}
           </ul>
         </section>
       </div>
@@ -34,29 +43,61 @@ const Today = () => {
   );
 };
 
+type TaskProps = {
+  title: string;
+  isCompleted: boolean;
+  _id: Id<"tasks">;
+  bold?: boolean;
+  underline?: boolean;
+  italic?: boolean;
+  highlight?: boolean;
+  highlightColor?: string;
+};
+
 const Task = ({
   task,
-  isCompleted,
+  check,
 }: {
-  task: string;
-  isCompleted: boolean;
+  task: TaskProps;
+  check: ReactMutation<
+    FunctionReference<
+      "mutation",
+      "public",
+      {
+        id: Id<"tasks">;
+        markAs: boolean;
+      },
+      null
+    >
+  >;
 }) => {
+  const handelClick = (id: Id<"tasks">, markAs: boolean) => {
+    const promise = check({ id, markAs });
+    toast.promise(promise, {
+      loading: "Loading",
+      success: "Success",
+      error: "Error",
+    });
+  };
+
   return (
     <li className=" hover:bg-secondary mt-2 rounded-md group/task transition-colors duration-300 ">
       <div className="flex items-center justify-between">
-        <div className="flex items-center gap-2">
-          <div role="button" className="cursor-pointer py-2 px-2 rounded">
+        <div className="flex items-center gap-1 flex-1">
+          <div
+            onClick={() => handelClick(task._id, !task.isCompleted)}
+            role="button"
+            className="cursor-pointer py-2 px-2 rounded">
             <svg
               xmlns="http://www.w3.org/2000/svg"
-              xmlns:xlink="http://www.w3.org/1999/xlink"
+              xmlnsXlink="http://www.w3.org/1999/xlink"
               aria-hidden="true"
               role="img"
-              size="18"
               width="18"
               height="18"
               viewBox="0 0 24 24"
-              className={cn(isCompleted && "text-primary")}>
-              {isCompleted ? (
+              className={cn(task.isCompleted && "text-primary")}>
+              {task.isCompleted ? (
                 <path
                   fill="currentColor"
                   d="M12 2c5.523 0 10 4.477 10 10s-4.477 10-10 10S2 17.523 2 12S6.477 2 12 2m3.22 6.97l-4.47 4.47l-1.97-1.97a.75.75 0 0 0-1.06 1.06l2.5 2.5a.75.75 0 0 0 1.06 0l5-5a.75.75 0 1 0-1.06-1.06"></path>
@@ -67,12 +108,17 @@ const Task = ({
               )}
             </svg>
           </div>
-          <div>
+          <div className="w-full">
             <p
-              className={
-                isCompleted ? "text-card-foreground/50 line-through" : ""
-              }>
-              {task}
+              style={{ backgroundColor: task.highlightColor }}
+              className={cn(
+                " font-[400] px-1 rounded",
+                task.bold && "font-bold",
+                task.underline && "underline",
+                task.italic && "italic",
+                task.isCompleted && "text-card-foreground/50 line-through"
+              )}>
+              {task.title}
             </p>
           </div>
         </div>
