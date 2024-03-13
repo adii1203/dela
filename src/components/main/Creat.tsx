06c3c994fc61dc1299ae8f046c8bdd0a5d2a6193
adii1203 <aditya32ft@gmail.com>
2024-03-14 01:11:30 +0530

    Add Creat component for creating new tasks

---
+++ src/components/main/Creat.tsx
@@ -0,0 +1,51 @@
+import { CornerDownLeft, Plus } from "lucide-react";
+import { Input } from "../ui/input";
+import { useState } from "react";
+import { useMutation } from "convex/react";
+import { api } from "./../../../convex/_generated/api";
+import { toast } from "sonner";
+
+const Creat = () => {
+  const [data, setdata] = useState("");
+  const creatTask = useMutation(api.tasks.creatTask);
+
+  const handelTask = () => {
+    if (data === "") {
+      return;
+    }
+    const promise = creatTask({ title: data, isCompleted: false });
+    toast.promise(promise, {
+      loading: "Creating...",
+      success: "Task created",
+      error: "Failed to create task",
+    });
+    setdata("");
+  };
+
+  const handelKeyDown = (e: React.KeyboardEvent<HTMLInputElement>) => {
+    if (e.key === "Enter") {
+      handelTask();
+    }
+  };
+
+  return (
+    <div className="mx-auto max-w-[40rem] mt-8 flex items-center px-3 border border-border rounded-[0.5rem]">
+      <Plus width={18} height={18} />
+      <Input
+        onKeyDown={handelKeyDown}
+        value={data}
+        onChange={(e) => setdata(e.target.value)}
+        type="text"
+        placeholder="Create new task"
+        className="border-none font-Lato tracking-widest font-semibold"
+      />
+      <CornerDownLeft
+        onClick={handelTask}
+        strokeWidth={1.25}
+        className="w-10 rounded cursor-pointer hover:bg-primary/80 bg-primary text-primary-foreground p-1 transition-colors duration-300"
+      />
+    </div>
+  );
+};
+
+export default Creat;
