5368042e29c8650746e2d977c087c9b849f9f54d
adii1203 <aditya32ft@gmail.com>
2024-03-14 23:10:36 +0530

    Add new mutations for text formatting and task deletion

--- convex/tasks.ts
+++ convex/tasks.ts
@@ -5,6 +5,7 @@ export const creatTask = mutation({
   args: {
     title: v.string(),
     isCompleted: v.optional(v.boolean()),
+    type: v.string(),
   },
   handler: async (ctx, args) => {
     const identity = await ctx.auth.getUserIdentity();
@@ -21,6 +22,7 @@ export const creatTask = mutation({
       highlight: false,
       highlightColor: "",
       url: "",
+      type: args.type,
     });
 
     return task;
@@ -41,6 +43,61 @@ export const markCompleted = mutation({
   },
 });
 
+export const bold = mutation({
+  args: {
+    id: v.id("tasks"),
+    bold: v.boolean(),
+  },
+  handler: async (ctx, args) => {
+    const identity = await ctx.auth.getUserIdentity();
+    if (identity === null) {
+      throw new Error("Unauthorized");
+    }
+    await ctx.db.patch(args.id, { bold: args.bold });
+  },
+});
+
+export const underline = mutation({
+  args: {
+    id: v.id("tasks"),
+    underline: v.boolean(),
+  },
+  handler: async (ctx, args) => {
+    const identity = await ctx.auth.getUserIdentity();
+    if (identity === null) {
+      throw new Error("Unauthorized");
+    }
+    await ctx.db.patch(args.id, { underline: args.underline });
+  },
+});
+
+export const italic = mutation({
+  args: {
+    id: v.id("tasks"),
+    italic: v.boolean(),
+  },
+  handler: async (ctx, args) => {
+    const identity = await ctx.auth.getUserIdentity();
+    if (identity === null) {
+      throw new Error("Unauthorized");
+    }
+    await ctx.db.patch(args.id, { italic: args.italic });
+  },
+});
+
+export const deleteTask = mutation({
+  args: {
+    id: v.id("tasks"),
+  },
+  handler: async (ctx, args) => {
+    const identity = await ctx.auth.getUserIdentity();
+    if (identity === null) {
+      throw new Error("Unauthorized");
+    }
+    await ctx.db.delete(args.id);
+  },
+});
+
 export const getAllTask = query({
   args: {},
   handler: async (ctx) => {
@@ -53,7 +110,6 @@ export const getAllTask = query({
       .withIndex("by_userId", (q) => q.eq("userId", identity.subject))
       .order("desc")
       .collect();
-    console.log(allTask);
 
     return allTask;
   },
